2d249152da35643e0c634f14ce20c63164a7b5ba
CS-5 <me@carsonseese.com>
2022-04-13 11:05:06 -0400

    Basic router setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import { handleRequest } from "./handler";
+import { Router } from "itty-router";
 
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+// now let's create a router (note the lack of "new")
+const router = Router();
+
+// GET
+router.get("/hello", () => new Response("Hello World!"));
+
+// 404 for everything else
+router.all("*", () => new Response("Not Found.", { status: 404 }));
+
+// attach the router "handle" to the event handler
+addEventListener("fetch", (event) =>
+  event.respondWith(router.handle(event.request))
+);
